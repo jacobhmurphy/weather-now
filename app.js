c38b70e3021ee70e5ec5313158c5a3cf1d63e8d0
jacobhmurphy <mrjacobmurphy@gmail.com>
2019-11-04 10:42:15 -0500

    nodemon

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const app = express()
  
 app.get('/', function (req, res) {
-  res.send('Hello World')
+  res.send('Hello World!!!')
 })
  
-app.listen(3000)
+app.listen(3000, function() {
+    console.log('Server is running on port 3000...')
+})
