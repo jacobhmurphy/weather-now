24eaa3c9fe87eb69dd4646695db290ee342031ae
jacobhmurphy <mrjacobmurphy@gmail.com>
2019-11-05 10:47:41 -0500

    adding body-parser

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
 // setting view engine
@@ -8,13 +9,18 @@ app.set('view engine', 'ejs')
 // middleware
 
 app.use(express.static('./public'))
+app.use(bodyParser.urlencoded({extended: false}))
 
 // routing: GET and 
 
 app.get('/', function (req, res) {
   res.render('home.ejs')
 })
+
+app.post('/test-action', function (req, res) {
+  console.log(req)
+})
  
 app.listen(3000, function() {
-    console.log('Server is running on port 3000...')
+  console.log('Server is running on port 3000...')
 })
