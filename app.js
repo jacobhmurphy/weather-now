d93e99883cf040b386f847ece0f16685d8f69691
jacobhmurphy <mrjacobmurphy@gmail.com>
2019-11-05 11:50:07 -0500

    changing message based off user input

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ app.use(bodyParser.urlencoded({extended: false}))
 // routing: GET and 
 
 app.get('/', function (req, res) {
-  res.render('home.ejs')
+  res.render('home.ejs', { weather: "Enter a location in the text box to see your local weather." })
 })
 
 app.post('/', function (req, res) {
   console.log(req.body.location)
-  // res.end()
+  let locationWeather = "The weather in " + req.body.location + " is 75F and sunny."
+  let err = false
+  res.render('home.ejs', { weather: locationWeather, errorMessage: err })
 })
  
 app.listen(3000, function() {
