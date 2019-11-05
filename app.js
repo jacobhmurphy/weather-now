efb3001c9ff9f37268a2486b82d953cba9c16297
jacobhmurphy <mrjacobmurphy@gmail.com>
2019-11-05 15:55:50 -0500

    connecting app to openweathermap api

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
+const request = require('request')
 const app = express()
+const apiKey = process.env.APIKEY
 
 // setting view engine
 
@@ -14,14 +17,32 @@ app.use(bodyParser.urlencoded({extended: false}))
 // routing: GET and 
 
 app.get('/', function (req, res) {
-  res.render('home.ejs', { weather: "Enter a location in the text box to see your local weather." })
+  res.render('home.ejs', { weather: null, error: null })
 })
 
 app.post('/', function (req, res) {
   console.log(req.body.location)
-  let locationWeather = "The weather in " + req.body.location + " is 75F and sunny."
   let err = false
-  res.render('home.ejs', { weather: locationWeather, errorMessage: err })
+  weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q='+req.body.location+'&units=metric&APPID='+apiKey
+  request(weatherURL, function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    /* console.log('body:', body); // Print the HTML for the Google homepage. */
+    if (error) {
+      res.render('home.ejs', { weather: null, error: "error please try again" })
+    } else {
+      let weather = JSON.parse(body)
+      // console.log(weather.weather)
+      if (weather.main == undefined) {
+        res.render('home.ejs', { weather: null, error: "error please try again" })
+      } else {
+        var locationWeather = "The weather in " + req.body.location + " is " + weather.main.temp + "\xB0C."
+        console.log(weather.main.temp)
+        res.render('home.ejs', { weather: locationWeather, error: err })
+      }
+    }
+  });
+  // res.render('home.ejs', { weather: locationWeather, error: err })
 })
  
 app.listen(3000, function() {
